e61891f1cb7a9aeedd6343ff2c14b212efd2e2aa
kasane <1259117394@qq.com>
2021-12-02 09:15:35 +0800

    将学习率调整为0.1

--- Task01/实验一基于感知机的鸢尾花分类.py
+++ Task01/实验一基于感知机的鸢尾花分类.py
@@ -86,14 +86,13 @@ def train():
     # T - Y得到100个的标签值与预测值的误差E。形状为(100,1)
     E = T - Y
     # X的形状为(100,3)
-    # X.T表示X的转置矩阵，形状为(3,100)
     # 我们一共有100个数据，每个数据3个特征的值。定义第i个数据的第j个特征值为xij
     # 如第1个数据，第2个值为x12
     # X.T.dot(E)为一个3行1列的数据：
     # 第1行等于：x0_0×e0+x1_0×e1+x2_0×e2+x3_0×e3+...+x99_0×e99，它会调整权值W0
     # 第2行等于：x0_1×e0+x1_1×e1+x2_1×e2+x3_1×e3+...+x99_1×e99，它会调整权值W1
     # 第3行等于：x0_2×e0+x1_2×e1+x2_2×e2+x3_2×e3+...+x99_2×e99，它会调整权值W2
-    # X.shape表示X的形状X.shape[0]得到X的行数，表示有多少个数据
+    # X.shape[0]得到X的行数，表示有多少个数据
     # X.shape[1]得到列数，表示每个数据有多少个特征值。
     delta_W = lr * (X.T.dot(E)) / X.shape[0]
     W = W + delta_W
@@ -102,23 +101,24 @@ def train():
 # # 画图函数
 def draw():
     plt.clf()
-    plt.xlim(0, 6)#x轴上的最小值和最大值
-    plt.ylim(0, 2)#y轴上的最小值和最大值
-    plt.title(u'Perceptron感知器 epoch:%d\n W0:%f W1:%f W2:%f' %(i+1,W[0],W[1],W[2]), fontsize=15)
+    plt.xlim(0, 6)  # x轴上的最小值和最大值
+    plt.ylim(0, 2)  # y轴上的最小值和最大值
+    plt.title(u'Perceptron感知器 epoch:%d\n W0:%f W1:%f W2:%f' % (i+1, W[0], W[1], W[2]), fontsize=15)
        
     plt.xlabel('petal length 花瓣长度', fontsize=13)
     plt.ylabel('petal width 花瓣宽度', fontsize=13)
     plt.plot(X[:50, 1], X[:50, 2], '^', color='red', label='Setosa山鸢尾')
-    # 用黄色的点来画出负样本
+    # 用黄色的点来画出负样本 ？
     plt.plot(X[50:100, 1], X[50:100, 2], 's', color='blue', label='Versicolour变色鸢尾')
     plt.plot(2.5, 1, '+', color='black', label='待预测点')
-    
+
+    # k和d是啥？
     k = - W[1] / W[2]
     d = - W[0] / W[2]
     # 设定两个点
-    xdata = (0,6)
-    # 通过两个点来确定一条直线，用红色的线来画出分界线
-    plt.plot(xdata,xdata * k + d, 'black', linewidth=3)
+    xdata = (0, 6)
+    # 通过两个点来确定一条直线，用红色的线来画出分界线 black？
+    plt.plot(xdata, xdata * k + d, 'black', linewidth=3)
     plt.legend()
     ######################################################以下绘制决策面两边的颜色，不要求掌握
     # 生成决策面 
@@ -140,12 +140,12 @@ def draw():
 
 # # 训练100次
 for i in range(1000):
-    if(i==0):    #特地画出未经训练的初始图像，以方便理解
+    if(i == 0):    # 特地画出未经训练的初始图像，以方便理解
         draw()
-        plt.pause(5)  #停留两秒，这是分类直线最初的位置，取决于W的初始值，是人为决定的超参数
-    train()      #更新一次权值
-    draw()       #画出更新一次权值后的图像
-    Y = np.sign(np.dot(X,W)) 
+        plt.pause(5)  # 停留两秒，这是分类直线最初的位置，取决于W的初始值，是人为决定的超参数
+    train()      # 更新一次权值
+    draw()       # 画出更新一次权值后的图像
+    Y = np.sign(np.dot(X, W))
     # .all()表示Y中的所有值跟T中所有值都对应相等，结果才为真
     if(Y == T).all(): 
         print('Finished')
